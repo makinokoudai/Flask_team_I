5394a8f9111d4f316b9931abaa9d98385f564e0c
makino <makino.kodai.ni@mynavi.jp>
2023-06-01 07:29:45 +0000

    flashまで

--- Makino/application/flask_blog/config.py
+++ Makino/application/flask_blog/config.py
@@ -1,5 +1,6 @@
 
 
 DEBUG = True
+SECRET_KEY = 'secret key'
 USERNAME = 'john'
 PASSWORD = 'due123'
--- Makino/application/flask_blog/views.py
+++ Makino/application/flask_blog/views.py
@@ -3,6 +3,8 @@ from flask import request,redirect,url_for,render_template,flash,session
 
 @app.route('/')
 def show_entries():
+    if not session.get('logged_in'):
+        return redirect('/login')
     return render_template("entries/index.html")
 
 
@@ -10,15 +12,19 @@ def show_entries():
 def login():
     if request.method == 'POST':
         if request.form['username']!= app.config['USERNAME']:
-            print('ユーザ名が異なります')
+            flash('ユーザ名が異なります')
         elif request.form['password'] != app.config['PASSWORD']:
-            print('パスワードが異なります')
+            flash('パスワードが異なります')
         else:
+            session['logged_in'] = True
+            flash('ログインしました')
             return redirect('/')
     return render_template('login.html')
 
 @app.route('/logout')
 def logout():
+    session.pop('logged_in',None)
+    flash('ログアウトしました')
     return redirect('/')
 
 
