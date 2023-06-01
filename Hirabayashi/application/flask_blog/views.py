7ba9c61b1966183f1d838b39ae7c654c7ea9b8dd
KentoHirabayashi <hirabayashi.kento.qr@mynavi.jp>
2023-06-01 06:00:07 +0000

    add login and logout form

--- Hirabayashi/application/flask_blog/views.py
+++ Hirabayashi/application/flask_blog/views.py
@@ -6,6 +6,17 @@ from flask_blog import app
 def show_entries():
     return render_template('entries/index.html')
 
-@app.route('/conban')
-def show_entries1():
-    return "Conban World!"
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            print('ユーザー名が異なります')
+        elif request.form['password'] != app.config['PASSWORD']:
+            print('パスワードが異なります')
+        else:
+            return redirect('/')
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    return redirect('/')
