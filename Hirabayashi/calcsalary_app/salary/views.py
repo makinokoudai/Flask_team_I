afa03060ea8d2420b2c641af9db26f47084460f0
KentoHirabayashi <hirabayashi.kento.qr@mynavi.jp>
2023-06-02 04:59:14 +0000

    add Comment

--- Hirabayashi/calcsalary_app/salary/views.py
+++ Hirabayashi/calcsalary_app/salary/views.py
@@ -9,12 +9,15 @@ def input():
 @app.route("/output", methods=["GET","POST"])
 def output():
     if request.method == "POST":
+        # 未入力の検出
         if request.form["salary"] == "":
             flash("給与が未入力です。入力してください。")
             return redirect(url_for("input"))
         
+        # 入力する値がある場合の処理を行う
         salary = int(request.form['salary'])
 
+        # 計算をしない場合の処理を記述
         if salary >= 10000000000:
             flash('給与の最大入力範囲を超えています。')
             session['input_data'] = salary  
@@ -24,6 +27,7 @@ def output():
             session['input_data'] = salary  
             return redirect(url_for("input"))
         
+        # 正常系の処理
         session.pop('input_data', None)  
         if salary > 1000000: 
             tax_amount = int((salary - 1000000) * 0.2 + (1000000 * 0.1))
