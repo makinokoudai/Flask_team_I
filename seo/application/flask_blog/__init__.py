50fc2071ab1ae66c5210a8a7a94782c581c8e5f7
TakenobuSeo <seo.takenobu.bl@mynavi.jp>
2023-06-02 07:44:05 +0000

    新規投稿画面作成

--- seo/application/flask_blog/__init__.py
+++ seo/application/flask_blog/__init__.py
@@ -6,5 +6,5 @@ app.config.from_object('flask_blog.config')
 
 db = SQLAlchemy(app)
 
-import seo.application.flask_blog.views.views, seo.application.flask_blog.views.entries
-# from flask_blog.views import views, entries
+# import seo.application.flask_blog.views.views, seo.application.flask_blog.views.entries
+from flask_blog.views import views, entries
