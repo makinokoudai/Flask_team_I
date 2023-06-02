d0c4d710858e67442170a75755aca25d22d99c76
makino <makino.kodai.ni@mynavi.jp>
2023-06-02 08:18:48 +0000

    教科書28章手前まで

--- Makino/application/flask_blog/models/entries.py
+++ Makino/application/flask_blog/models/entries.py
@@ -3,15 +3,16 @@ from datetime import datetime
 
 class Entry(db.Model):
     __tablename__ = 'entries'
-    id = db.Column(db.Integer,primary_key = True)
-    title = db.Column(db.String(50),unique = True)
+
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(50), unique=True)
     text = db.Column(db.Text)
     created_at = db.Column(db.DateTime)
-    
-    def __init__(self,title = None ,text = None):
+
+    def __init__(self, title=None, text=None):
         self.title = title
         self.text = text
         self.created_at = datetime.utcnow()
-        
+
     def __repr__(self):
-        return '<Entry id:{}title:{}text{}>'.format(self.id,self.title,self.text)
+        return f'<Entory id:{self.id} title:{self.title} text:{self.text}>'
