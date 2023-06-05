81c279bab133ea7bab9751064e8716f25fad49c1
KentoHirabayashi <hirabayashi.kento.qr@mynavi.jp>
2023-06-05 02:37:32 +0000

    add decolator function to check login

--- Hirabayashi/application/flask_blog/views/views.py
+++ Hirabayashi/application/flask_blog/views/views.py
@@ -1,5 +1,8 @@
 from flask  import  request, redirect, url_for, render_template, flash, session
+from functools import wraps
+
 from flask_blog import app
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -19,3 +22,11 @@ def logout():
     session.pop('logged_in', None)
     flash('ログアウトしました')
     return redirect(url_for('show_entries'))
+
+def login_required(view):
+    @wraps(view)
+    def inner(*args, **kwargs):
+        if not session.get('logged_in'):
+            return redirect(url_for('login'))
+        return view(*args, **kwargs)
+    return inner
