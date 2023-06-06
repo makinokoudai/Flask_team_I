6504cf0dbf02342ee8b0588fa8fe0b8086442ba6
TakenobuSeo <seo.takenobu.bl@mynavi.jp>
2023-06-06 00:00:04 +0000

    祝日登録と削除のボタンの処理を区別

--- seo/holiday_app/holiday/views/maintenance_date.py
+++ seo/holiday_app/holiday/views/maintenance_date.py
@@ -5,12 +5,22 @@ from holiday.models.mst_holiday import Holiday
 
 @app.route('/new', methods=['POST'])
 def add_input():
-    holiday = Holiday(
-        holi_date = request.form['holiday'],
-        holi_text = request.form['holiday_text']
-    )
-    db.session.add(holiday)
-    db.session.commit()
-    flash('祝日が追加されました')
+    # if Holiday.query.get()
+    if 'write' in request.form:
+        holiday = Holiday(
+            holi_date = request.form['holiday'],
+            holi_text = request.form['holiday_text']
+        )
+        db.session.add(holiday)
+        db.session.commit()
+        flash('祝日が追加されました')
+        
+    elif 'delete' in request.form:
+        holiday = Holiday.query.get(request.form['holiday'])
+        db.session.delete(holiday)
+        db.session.commit()
+        flash('祝日が削除されました')
+        return redirect(url_for('show_input'))
+
 
     return render_template('result.html')
